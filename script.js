1e2cfe7c2ba70e4bec1b0d321d9e489b9f363c43
e-4e8 <7hichan7@gmail.com>
2025-02-15 13:35:26 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const addTaskButton = document.getElementById('add-task-button');
+const addTaskForm = document.getElementById('add-task-form');
+
+addTaskButton.addEventListener('click', () => {
+    addTaskForm.style.display = 'block';
+});
