d98481f8569c8fa1f24ea17513cf2f7221f12bee
Aya-8 <dreamscometrue.h1821@gmail.com>
2025-02-15 14:23:16 +0900

    Update count.js

--- count.js
+++ count.js
@@ -1,12 +1,20 @@
 function startTimer() {
-  const time = document.getElementById('time').value;
+  const hours = document.getElementById('hours').value || 0;
+  const minutes = document.getElementById('minutes').value || 0;
+  const seconds = document.getElementById('seconds').value || 0;
+
+ //const task = document.getElementById('name');//
+  
+  // 入力された時間、分、秒をミリ秒に変換して合計
+  const totalTime = (parseInt(hours) * 3600 + parseInt(minutes) * 60 + parseInt(seconds)) * 1000;
+
   const messageElement = document.getElementById('message');
   messageElement.textContent = '';
-  
+
   setTimeout(function() {
-    messageElement.textContent = '時間が経過しました！';
+    messageElement.textContent = '$('name')をやる時間になったよ！';
     blinkMessage();
-  }, time * 1000);
+  }, totalTime);
 }
 
 function blinkMessage() {
